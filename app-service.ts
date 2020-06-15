f07dd572aaed46f42f963d61cab137866490db4e
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-06-15 11:07:44 +0530

    updated

---
+++ app-service.ts
@@ -0,0 +1,8 @@
+
+export interface StringManipulationService {
+  arrayMultiply(myArray : any) : Array<Number>;
+  arraySeparate(myArray : any) : Array<string>;
+  arraySplit(myString : string) : Array<number>;
+  arraySort(myArray : any) : Array<string>;
+  arrayReplace(myArray : any) : Array<Number>;
+}
